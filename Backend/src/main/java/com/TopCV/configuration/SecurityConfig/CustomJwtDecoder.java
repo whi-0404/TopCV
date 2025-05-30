48f191700e3f59262ef94d31d4f566091c6255e3
quanghuy <22520564@uit.edu.vn>
2025-05-30 15:16:09 +0700

    feat: security config

---
+++ Backend/src/main/java/com/TopCV/configuration/SecurityConfig/CustomJwtDecoder.java
@@ -0,0 +1,53 @@
+package com.TopCV.configuration.SecurityConfig;
+
+import com.TopCV.dto.request.IntrospectRequest;
+import com.TopCV.exception.JwtAuthenticationException;
+import com.TopCV.service.impl.AuthenticationServiceImpl;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Objects;
+
+@Component
+@Log4j2
+public class CustomJwtDecoder implements JwtDecoder {
+    @Value("${jwt.signerKey}")
+    private String signerKey;
+
+    @Autowired
+    private AuthenticationServiceImpl authenticationService;
+
+    private NimbusJwtDecoder nimbusJwtDecoder = null;
+
+    @Override
+    public Jwt decode(String token) throws JwtException {
+
+        try {
+            var response = authenticationService.introspect(
+                    IntrospectRequest.builder().token(token).build());
+
+            if (!response.isValid()) {
+                throw new JwtAuthenticationException(response.getErrorCode());
+            }
+        } catch (Exception   e) {
+            throw new JwtException(e.getMessage());
+        }
+
+        if (Objects.isNull(nimbusJwtDecoder)) {
+            SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+            nimbusJwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec)
+                    .macAlgorithm(MacAlgorithm.HS512)
+                    .build();
+        }
+
+        return nimbusJwtDecoder.decode(token);
+    }
+}
